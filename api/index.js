ecb6006bb0ece78401d90f756545392b64a40201
Ojee Afolabi <sylvesterojay@gmail.com>
2025-04-08 17:47:17 +0100

    creating signup logic

--- api/index.js
+++ api/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 5000;
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+app.post("/signup", (req, res) => {
+  res.send("User signed up successfully!");
 });
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// mongodb+srv://afolabiojee:Jayson@931005@cluster0.irzglmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  async function signup(e) {
+    e.preventDefault();
+    await fetch("http://localhost:5000/signup", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
   return (
-    <form>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+    <form onSubmit={signup}>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button type="submit">Sign Up</button>
     </form>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
