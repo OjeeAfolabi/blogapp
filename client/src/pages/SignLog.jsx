8aa4c5f47ed385e77ba93d17fc35aee842bc97fc
Ojee Afolabi <sylvesterojay@gmail.com>
2025-09-12 14:55:00 +0100

    test

--- client/src/pages/SignLog.jsx
+++ client/src/pages/SignLog.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const SignLog = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-darkblue to-lightblue flex flex-col items-center justify-center p-6">
-      <div className=""></div>
+      <div className="border-4 border-white rounded-md">
+        <h1>Login</h1>
+        <div>
+          <input type="text" placeholder="Email" required />
+        </div>
+        <div>
+          <input type="password" placeholder="Password" required />
+        </div>
+      </div>
     </div>
   );
 };
